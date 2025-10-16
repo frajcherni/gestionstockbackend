d6fb8a68cfe799f449b2100277f6808fbbe710ab
Cherni Fraj <frajcherni81@gmail.com>
2025-10-17 00:26:59 +0100

    description du changement

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,11 +1,11 @@
 // routes/auth.js
 const express = require("express");
 const router = express.Router();
-const { register, login , updateProfile } = require("../controllers/authController");
-const authController = require('../controllers/authController');
+const { register, login , updateProfile } = require("../controllers/AuthController");
+const AuthController = require('../controllers/AuthController');
 
 router.post("/register", register);
 router.post("/login", login);
-router.put('/profile', authController.upload.single('company_logo'), authController.updateProfile);
+router.put('/profile', AuthController.upload.single('company_logo'), AuthController.updateProfile);
 
 module.exports = router;
