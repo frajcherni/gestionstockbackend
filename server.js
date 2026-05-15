b679c248c949d25b6c11cf5d588f17f06edb00e8
frajcherni <frajcherni81@gmail.com>
2026-05-15 23:52:56 +0100

    fff

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Something broke!");
+  res.status(500).send("Oups Something broke!");
 });
 
 app.use("/api/articles", articleRoutes);
